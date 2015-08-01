354e1cfbe3b9b16b4cb6f88db436022621fa0373
softwarespot <softwarespotapps@gmail.com>
2015-08-01 08:18:12 +0300

    Remove undefined

--- module_pattern.js
+++ module_pattern.js
@@ -1,20 +1,20 @@
 var Namespace = (function (window, document, undefined) {
     // Constants
-    
+
     // Configuration
     var config = {
 
     };
-    
+
     // Fields
-    
+
     // Methods
     var init = function () {
 
     };
-    
+
     // Public API
     return {
 		// outer: inner
     };
-})(window, document, undefined);
+})(window, document);
