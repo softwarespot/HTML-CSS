e796c3256826295816e5b0728143c438d6104a9e
softwarespot <softwarespotapps@gmail.com>
2016-06-04 11:07:21 +0300

    Refactored to use a Promise

--- jsonp.js
+++ jsonp.js
@@ -1,66 +1,77 @@
-/*global console*/
+/*global console, Promise*/
 
-var request = (function request(document, setTimeout) {
-    var api = {
+var jsonp = (function jsonpModule(document, Object, Promise, encodeURIComponent, setTimeout) {
+    var _api = {
         callbacks: {},
-        jsonp: jsonp,
+        get: get,
     };
     var _id = 0;
+    var _noop = function noop() {};
 
-    return api;
+    return _api;
 
     /**
      * Simple JSON-P implementation
      */
-    function jsonp(url, options) {
-        var noop = function noop() {};
-        options.on.failure = options.on.failure || noop;
-        options.on.success = options.on.success || noop;
-        options.on.timeout = options.on.timeout || noop;
+    function get(url, params) {
+        var error = _noop;
+        var success = _noop;
 
-        var callback = 'req' + _id++;
-        var params = options.params;
-        params += '&callback=request.callbacks.' + callback;
+        var promise = new Promise(function promise(resolve, reject) {
+            success = resolve;
+            error = reject;
+        });
+
+        var callback = 'get' + _id++;
+
+        // Extend with the callback function parameter
+        params = Object.assign(params || {}, {
+            callback: 'jsonp.callbacks.' + callback,
+        });
 
         // Create script element
         var script = document.createElement('script');
         script.async = true;
-        script.src = url + '?' + params;
+        script.src = url + '?' + Object.keys(params).map(function mapKeys(key) {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+        }).join('&');
 
-        // Set callback functions
-        script.addEventListener('error', function error() {
-            options.on.failure();
+        // Set callback function(s)
+        script.addEventListener('error', function errorEvent() {
             _destroy();
+            error();
         });
 
-        api.callbacks[callback] = function success(response) {
-            options.on.success(response);
+        _api.callbacks[callback] = function successEvent(response) {
             _destroy();
+            success(response);
         };
-
-        setTimeout(function timeout() {
-            options.on.timeout();
-            _destroy();
-        }, options.timeout || 10000);
 
         // Inject script
         setTimeout(function setTimeout() {
             document.head.appendChild(script);
-        }, 5);
+        }, 2);
 
-        // Clear the global callback function
+        return promise;
+
+        // Destroy the JSON-P csllback function and script element node
         function _destroy() {
-            api.callbacks[callback] = noop;
+            _api.callbacks[callback] = _noop;
+            script.parentNode.removeChild(script);
         }
     }
-}(window.document, window.setTimeout));
+}(
+    document,
+    Object,
+    Promise,
+    encodeURIComponent,
+    setTimeout
+));
 
-request.jsonp('https://api.ipify.org', {
-    params: 'format=jsonp',
-    on: {
-        success: function success(response) {
-            console.log(response);
-        },
-    },
-    timeout: 10000,
+jsonp.get('https://api.ipify.org', {
+    format: 'jsonp',
+}).then(function success(response) {
+    console.log(response);
+}).catch(function error() {
+    console.log('An error occurred. As to what that error was, it\'s hard to determine using JSON-P');
 });
